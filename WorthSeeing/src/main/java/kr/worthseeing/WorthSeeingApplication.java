dd7cf17f9e7211204929e4527c75d916ea9d2c62
kimhyoju123 <qjqmf217@naver.com>
2022-12-14 19:28:56 +0900

    First Upload

---
+++ WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
@@ -0,0 +1,13 @@
+package kr.worthseeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorthSeeingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorthSeeingApplication.class, args);
+	}
+
+}
---
+++ WorthSeeing/src/test/java/kr/worthseeing/WorthSeeingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.worthseeing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorthSeeingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
