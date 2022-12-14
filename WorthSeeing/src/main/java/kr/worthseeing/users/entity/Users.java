b7705beb75f7846b441e9c06a6270e7e1355f756
User <User@host.docker.internal>
2022-12-14 19:57:19 +0900

    entity package

---
+++ WorthSeeing/src/main/java/kr/worthseeing/users/entity/Users.java
@@ -0,0 +1,12 @@
+package kr.worthseeing.users.entity;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+//@ToString(exclude = {"member","replyList","fileList"})
+public class Users {
+
+}
