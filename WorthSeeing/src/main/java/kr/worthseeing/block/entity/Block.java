eb014e9ce2a2a4141e77ae6b0532904a7b7206c7
wlgns679 <qkr5778@naver.com>
2022-12-14 21:07:00 +0900

    entity upload

--- WorthSeeing/src/main/java/kr/worthseeing/block/entity/Block.java
+++ WorthSeeing/src/main/java/kr/worthseeing/block/entity/Block.java
@@ -1,7 +1,35 @@
 package kr.worthseeing.block.entity;
 
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Block {
 
+	@Id
+	@GeneratedValue
+	private int linkBoard_seq;
+	private int groupBlock_seq;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(updatable = false)
+	private Date startDate;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(updatable = false)
+	private Date endDate;
+	
 }
