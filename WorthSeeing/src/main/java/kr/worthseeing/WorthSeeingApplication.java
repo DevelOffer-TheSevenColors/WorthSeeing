137439b6133aefaf0323f648ee6741f4f96225ef
Hwanseokkkk <5038256@gmail.com>
2022-12-27 18:32:21 +0900

    codeChange  worthseeingApplication

--- WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
+++ WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
@@ -8,13 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class WorthSeeingApplication extends SpringBootServletInitializer{
+public class WorthSeeingApplication{
 	
-	 // 이 부분 추가(war하기위해 추가)
-		@Override
-		protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-			return builder.sources(WorthSeeingApplication.class);
-		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorthSeeingApplication.class, args);
