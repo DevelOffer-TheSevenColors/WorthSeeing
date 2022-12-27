486f0d601df290530c6ec0bb70e4db139ae7334a
Hwanseokkkk <5038256@gmail.com>
2022-12-27 23:44:15 +0900

    Upload WorthSeeingApplication

--- WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
+++ WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
@@ -2,11 +2,18 @@ package kr.worthseeing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class WorthSeeingApplication{
+public class WorthSeeingApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WorthSeeingApplication.class);
+    }
 	
 
 	public static void main(String[] args) {
