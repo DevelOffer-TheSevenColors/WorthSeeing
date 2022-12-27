b799956e1ec2e953a270f47cdf99529174a5fe65
Hwanseokkkk <5038256@gmail.com>
2022-12-27 17:56:01 +0900

    CodeChange POM.XML && WorthseeingApplication.java

--- WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
+++ WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
@@ -2,11 +2,19 @@ package kr.worthseeing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class WorthSeeingApplication {
+public class WorthSeeingApplication extends SpringBootServletInitializer{
+	
+	 // 이 부분 추가(war하기위해 추가)
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+			return builder.sources(WorthSeeingApplication.class);
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorthSeeingApplication.class, args);
