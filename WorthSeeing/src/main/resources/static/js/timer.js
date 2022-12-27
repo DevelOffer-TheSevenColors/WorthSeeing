6d47c820be4dbcc613ba5c6b8def24a030343b27
JungCheolHoon <cjswovkdnj12@naver.com>
2022-12-27 21:17:44 +0900

    asd

--- WorthSeeing/src/main/resources/static/js/timer.js
+++ WorthSeeing/src/main/resources/static/js/timer.js
@@ -4,7 +4,7 @@
 	console.log($("#reservation_seq").val());
   	
     var now = new Date();
-    var end = new Date(now.getFullYear(),now.getMonth(),now.getDate(),22,10,00);
+    var end = new Date(now.getFullYear(),now.getMonth(),now.getDate(),21,18,00);
     var open = new Date(now.getFullYear(),now.getMonth(),now.getDate(),21,12,00);
   
     var nt = now.getTime();
