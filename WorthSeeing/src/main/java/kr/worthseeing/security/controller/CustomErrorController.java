564176c3f7d7857ae7f53843ba8cee796f988045
JungCheolHoon <cjswovkdnj12@naver.com>
2022-12-28 22:28:01 +0900

    Merge branch 'main' of https://github.com/DevelOffer-TheSevenColors/WorthSeeing.git into main

--- WorthSeeing/src/main/java/kr/worthseeing/security/controller/CustomErrorController.java
+++ WorthSeeing/src/main/java/kr/worthseeing/security/controller/CustomErrorController.java
@@ -4,7 +4,7 @@
 //
 //import javax.servlet.RequestDispatcher;
 //import javax.servlet.http.HttpServletRequest;
-//
+////
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 //import org.springframework.http.HttpStatus;
