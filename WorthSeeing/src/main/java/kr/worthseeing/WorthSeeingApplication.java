76a28df8b2cf23d7c4e598e55cb700bad2a7cf35
Hwanseokkkk <5038256@gmail.com>
2022-12-28 22:40:38 +0900

    Application //

--- WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
+++ WorthSeeing/src/main/java/kr/worthseeing/WorthSeeingApplication.java
@@ -4,19 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan
 @EnableScheduling
 public class WorthSeeingApplication extends SpringBootServletInitializer {
 	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WorthSeeingApplication.class);
-    }
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorthSeeingApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WorthSeeingApplication.class);
 	}
 }
